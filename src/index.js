14685c8747e5243d8bb48fc8bad86e433576339c
yoann <yoann.ochietti@gmail.com>
2024-12-11 18:58:33 -0500

    Generate styled carousel as grid with hidden overflow
    Not dynamic/interactable yet. Includes some tests in the template that
    should eventually be moved to subfolder?

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import "./styles.css";
 
-console.log("Hello");
+const carousel = document.querySelector(".yo-carousel");
+const images = document.querySelectorAll(".yo-carousel img");
+
+const carouselWidth = carousel.clientWidth;
+const carouselHeight = carousel.clientHeight;
+
+const strip = document.createElement("div");
+strip.classList.add("carousel-strip");
+strip.style.setProperty("grid-template-rows", `${carouselHeight}px`);
+strip.style.setProperty("grid-auto-columns", `${carouselWidth}px`);
+
+const cells = [];
+for (let img of images) {
+  const cell = document.createElement("div");
+  cell.classList.add("carousel-cell");
+  cell.appendChild(img);
+  cells.push(cell);
+}
+strip.append(...cells);
+carousel.appendChild(strip);
