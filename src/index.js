f0200dab4e8068116ca150a68066f255dbc27a79
yoann <yoann.ochietti@gmail.com>
2024-12-12 11:56:26 -0500

    Allow multiple carousels in a page

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import "./styles.css";
 
-const carousel = document.querySelector(".yo-carousel");
-const images = document.querySelectorAll(".yo-carousel img");
+const carousels = document.querySelectorAll(".yo-carousel");
 
-const strip = document.createElement("div");
-strip.classList.add("carousel-strip");
+function init() {
+  for (let carousel of carousels) {
+    const images = carousel.querySelectorAll(".yo-carousel img");
+    const strip = document.createElement("div");
+    strip.classList.add("carousel-strip");
 
-const cells = [];
-for (let img of images) {
-  const cell = document.createElement("div");
-  cell.classList.add("carousel-cell");
-  cell.appendChild(img);
-  cells.push(cell);
+    const cells = [];
+    for (let img of images) {
+      const cell = document.createElement("div");
+      cell.classList.add("carousel-cell");
+      cell.appendChild(img);
+      cells.push(cell);
+    }
+    strip.append(...cells);
+    carousel.appendChild(strip);
+  }
 }
-strip.append(...cells);
-carousel.appendChild(strip);
+
+init();
