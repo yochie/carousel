e9899b999012a88fde07f67c8eab6b78d3f30a73
yoann <yoann.ochietti@gmail.com>
2024-12-12 11:49:49 -0500

    Size strip based on parent in css

--- src/index.js
+++ src/index.js
@@ -3,13 +3,8 @@ import "./styles.css";
 const carousel = document.querySelector(".yo-carousel");
 const images = document.querySelectorAll(".yo-carousel img");
 
-const carouselWidth = carousel.clientWidth;
-const carouselHeight = carousel.clientHeight;
-
 const strip = document.createElement("div");
 strip.classList.add("carousel-strip");
-strip.style.setProperty("grid-template-rows", `${carouselHeight}px`);
-strip.style.setProperty("grid-auto-columns", `${carouselWidth}px`);
 
 const cells = [];
 for (let img of images) {
